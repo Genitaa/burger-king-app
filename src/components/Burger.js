0c43b03dff300a43de618dba167a270c137c7efb
Genita Abazi <genitaabazi@hotmail.com>
2020-10-07 00:27:24 +0200

    Added styling to the Burger component using Bootstrap grid and BEM naming convention to the elements.

--- src/components/Burger.js
+++ src/components/Burger.js
@@ -1,44 +1,48 @@
 import React from 'react';
-import '../styles/components/_burger.scss';
 import { faCheck } from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import '../styles/components/_burger.scss';
 import logo from '../images/logo.png';
 
 function Burger() {
     return (
-        <div className="container">
-            <main className="rounded m-3">
+        <div className="container-fluid">
+            <main className="order">
                 <div className="row align-items-center">
                     <div className="col-md-3">
                         <div className="row">
                             <div className="col-md-4">
-                                <div className="w-50">
-                                    <p className="text-center">1</p>
+                                <div className="order__number">
+                                    <p>1</p>
                                 </div>
                             </div>
-                            <div className="col-md-4">
-                                <img src={logo} alt="Burger King Logo" className="img-fluid" />
+                            <div className="col-md-5">
+                                <div className="order__logo">
+                                    <img src={logo} alt="Burger King Logo" className="order__logo__img" />
+                                    <p>Burger King</p>
+                                </div>
                             </div>
-                            <div className="offset-md-1 col-md-3 align-self-center">
-                                <div className="bg-white border border-white rounded">
-                                    <p className="p-1 my-auto text-center">9/10</p>
+                            <div className="col-md-3 align-self-center">
+                                <div className="order__box order__box--border">
+                                    <p className="order__box__number">9/10</p>
                                 </div>
                             </div>
                         </div>
-                        <p className="text-center">Burger King</p>
                     </div>
                     <div className="col-md-3">
-                        <h6>Get 3 Burgers</h6>
-                        <p>Onions Free</p>
+                        <h6 className="order__description order__description--red">Get 3 Burgers</h6>
+                        <p className="order__description">Onions Free</p>
                     </div>
                     <div className="col-md-3">
-                        <p className="mb-1"><FontAwesomeIcon icon={faCheck} style={{color: "#217F86", marginRight: "10px"}} />Chicken Burger</p>
-                        <p className="mb-1"><FontAwesomeIcon icon={faCheck} style={{color: "#217F86", marginRight: "10px"}} />Wrapper Burger</p>
-                        <p className="mb-1"><FontAwesomeIcon icon={faCheck} style={{color: "#217F86", marginRight: "10px"}} />French Fries</p>
+                        <p className="order__details"><FontAwesomeIcon icon={faCheck} style={{color: "#00889e", marginRight: "10px"}} />Chicken Burger</p>
+                        <p className="order__details"><FontAwesomeIcon icon={faCheck} style={{color: "#00889e", marginRight: "10px"}} />Wrapper Burger</p>
+                        <p className="order__details"><FontAwesomeIcon icon={faCheck} style={{color: "#00889e", marginRight: "10px"}} />French Fries</p>
                     </div>
                     <div className="col-md-3">
-                        <a href="/">Visit</a>
-                        <button className="btn">ORDER</button>
+                        <div className="order__btns">
+                            <button className="btn order__btns__visit"><span>Visit</span></button>
+                            <button className="btn order__btns__order order__btns__order--bold"><span>ORDER</span></button>
+                        </div>
                     </div>
                 </div>
             </main>
