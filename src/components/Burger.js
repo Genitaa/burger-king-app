2a3b24cd1d821e5f5ec07356c06c64a914427088
Genita Abazi <genitaabazi@hotmail.com>
2020-10-07 14:22:30 +0200

    Added _mixins.scss file for media queries. Started making the app responsive on mobile.

--- src/components/Burger.js
+++ src/components/Burger.js
@@ -11,12 +11,12 @@ function Burger() {
                 <div className="row align-items-center">
                     <div className="col-md-3">
                         <div className="row">
-                            <div className="col-md-4">
+                            <div className="col-2 col-md-4">
                                 <div className="order__number">
                                     <p>1</p>
                                 </div>
                             </div>
-                            <div className="col-md-5">
+                            <div className="col-7 col-md-5">
                                 <div className="order__banter">
                                     <p className="order__banter__details">Super Offer</p>
                                 </div>
@@ -25,23 +25,23 @@ function Burger() {
                                     <p>Burger King</p>
                                 </div>
                             </div>
-                            <div className="col-md-3 align-self-center">
+                            <div className="col-3 align-self-center">
                                 <div className="order__box order__box--border">
                                     <p className="order__box__number">9/10</p>
                                 </div>
                             </div>
                         </div>
                     </div>
-                    <div className="col-md-3">
+                    <div className="offset-1 col-9 offset-md-0 col-md-3">
                         <h6 className="order__description order__description--red">Get 3 Burgers</h6>
                         <p className="order__description">Onions Free</p>
                     </div>
-                    <div className="col-md-3">
+                    <div className="offset-3 col-6 col-sm-4 offset-md-0 col-md-3">
                         <p className="order__details"><FontAwesomeIcon icon={faCheck} style={{color: "#00889e", marginRight: "10px"}} />Chicken Burger</p>
                         <p className="order__details"><FontAwesomeIcon icon={faCheck} style={{color: "#00889e", marginRight: "10px"}} />Wrapper Burger</p>
                         <p className="order__details"><FontAwesomeIcon icon={faCheck} style={{color: "#00889e", marginRight: "10px"}} />French Fries</p>
                     </div>
-                    <div className="col-md-3">
+                    <div className="offset-1 col-9 offset-md-0 col-md-3">
                         <div className="order__btns">
                             <button className="btn order__btns__visit"><span>Visit</span></button>
                             <button className="btn order__btns__order order__btns__order--bold"><span>ORDER</span></button>
