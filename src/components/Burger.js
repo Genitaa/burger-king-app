1cfead2f49be346d64d93b52e29021595d618d1e
Genita Abazi <genitaabazi@hotmail.com>
2020-10-07 22:11:57 +0200

    Changed some classes' names to more appropriate ones. Created a better scss structure.

--- src/components/Burger.js
+++ src/components/Burger.js
@@ -7,46 +7,46 @@ import logo from '../images/logo.png';
 function Burger() {
     return (
         <div className="container-fluid">
-            <main className="order">
+            <main className="product">
                 <div className="row align-items-center">
                     <div className="col-md-3">
                         <div className="row">
                             <div className="col-2 col-md-4">
-                                <div className="order__number">
+                                <div className="product__number">
                                     <p>1</p>
                                 </div>
                             </div>
                             <div className="col-7 col-md-4 col-lg-5">
-                                <div className="order__banter">
-                                    <p className="order__banter__details">Super Offer</p>
+                                <div className="banner">
+                                    <p className="banner__details">Super Offer</p>
                                 </div>
-                                <div className="order__logo">
-                                    <img src={logo} alt="Burger King Logo" className="order__logo__img" />
+                                <div className="logo">
+                                    <img src={logo} alt="Burger King Logo" className="logo__img" />
                                     <p>Burger King</p>
                                 </div>
                             </div>
                             <div className="col-3 col-md-4 col-lg-3 align-self-center">
-                                <div className="order__box order__box--border">
-                                    <p className="order__box__number">9/10</p>
+                                <div className="product__box product__box--border">
+                                    <p className="product__box__num">9/10</p>
                                 </div>
                             </div>
                         </div>
                     </div>
                     <div className="offset-1 col-9 offset-md-0 col-md-3">
-                        <h6 className="order__description order__description--red">Get 3 Burgers</h6>
-                        <p className="order__description">Onions Free</p>
+                        <h6 className="product__description product__description--red">Get 3 Burgers</h6>
+                        <p className="product__description">Onions Free</p>
                     </div>
                     <div className="col-md-3">
-                        <div className="order__details">
-                            <p className="order__details__p"><FontAwesomeIcon icon={faCheck} style={{color: "#00889e", marginRight: "10px"}} />Chicken Burger</p>
-                            <p className="order__details__p"><FontAwesomeIcon icon={faCheck} style={{color: "#00889e", marginRight: "10px"}} />Wrapper Burger</p>
-                            <p className="order__details__p"><FontAwesomeIcon icon={faCheck} style={{color: "#00889e", marginRight: "10px"}} />French Fries</p>
+                        <div className="product__details">
+                            <p className="product__details__p"><FontAwesomeIcon icon={faCheck} style={{color: "#00889e", marginRight: "10px"}} />Chicken Burger</p>
+                            <p className="product__details__p"><FontAwesomeIcon icon={faCheck} style={{color: "#00889e", marginRight: "10px"}} />Wrapper Burger</p>
+                            <p className="product__details__p"><FontAwesomeIcon icon={faCheck} style={{color: "#00889e", marginRight: "10px"}} />French Fries</p>
                         </div>
                     </div>
                     <div className="offset-1 col-10 offset-md-0 col-md-3">
-                        <div className="order__btns">
-                            <button className="btn order__btns__visit order__btns__visit--spacing"><span>Visit</span></button>
-                            <button className="btn order__btns__order order__btns__order--bold"><span>ORDER</span></button>
+                        <div className="btns">
+                            <button className="btn btns__visit btns__visit--spacing"><span>Visit</span></button>
+                            <button className="btn btns__order btns__order--bold"><span>ORDER</span></button>
                         </div>
                     </div>
                 </div>
