a9cadaa9e9fae32c3cb716847f86c207d0396650
Genita Abazi <genitaabazi@hotmail.com>
2020-10-07 15:51:12 +0200

    Completed the responsive styling to the Burger component.

--- src/components/Burger.js
+++ src/components/Burger.js
@@ -16,7 +16,7 @@ function Burger() {
                                     <p>1</p>
                                 </div>
                             </div>
-                            <div className="col-7 col-md-5">
+                            <div className="col-7 col-md-4 col-lg-5">
                                 <div className="order__banter">
                                     <p className="order__banter__details">Super Offer</p>
                                 </div>
@@ -25,7 +25,7 @@ function Burger() {
                                     <p>Burger King</p>
                                 </div>
                             </div>
-                            <div className="col-3 align-self-center">
+                            <div className="col-3 col-md-4 col-lg-3 align-self-center">
                                 <div className="order__box order__box--border">
                                     <p className="order__box__number">9/10</p>
                                 </div>
@@ -43,9 +43,9 @@ function Burger() {
                             <p className="order__details__p"><FontAwesomeIcon icon={faCheck} style={{color: "#00889e", marginRight: "10px"}} />French Fries</p>
                         </div>
                     </div>
-                    <div className="offset-1 col-9 offset-md-0 col-md-3">
+                    <div className="offset-1 col-10 offset-md-0 col-md-3">
                         <div className="order__btns">
-                            <button className="btn order__btns__visit"><span>Visit</span></button>
+                            <button className="btn order__btns__visit order__btns__visit--spacing"><span>Visit</span></button>
                             <button className="btn order__btns__order order__btns__order--bold"><span>ORDER</span></button>
                         </div>
                     </div>
