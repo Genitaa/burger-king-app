870cb3cca64058ba9b090482d9716ea9c16472b9
Genita Abazi <genitaabazi@hotmail.com>
2020-10-08 22:13:44 +0200

    Installed axios. Replaced test data with data from backend using Wordpress REST API.

--- src/components/ProductList.js
+++ src/components/ProductList.js
@@ -1,13 +1,39 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 import Product from './Product';
-import products from './ProductData';
 
 function ProductList() {
-    const mainProducts = products.map(product => <Product key={product.id} product={product} />);
+    const [products, setProducts] = useState([]);
+    const [loading, setLoading] = useState(false);
+
+    useEffect(() => {
+        setLoading(true);
+        axios.get('http://localhost/burger-king-app/wp-json/wp/v2/products')
+            .then(res => {
+                const newProducts = res.data.map(e => {
+                    const product = {
+                        id: 1,
+                        banner: e.acf.banner,
+                        logo: e.acf.logo.url,
+                        description: e.acf.description,
+                        quantity: e.acf.quantity,
+                        offer: e.acf.offer,
+                        offerDescription: e.acf.offer_description,
+                        details: e.acf.details.map(det => det.detail)
+                    };
+    
+                    return <Product key={product.id} product={product} />;
+                });
+    
+                setProducts(newProducts);
+                setLoading(false);
+            });
+    }, []);
+
 
     return (
         <div className="container-fluid">
-            {mainProducts}
+            {!loading ? products : "Loading"}
         </div>
     )
 }
